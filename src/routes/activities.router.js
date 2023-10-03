baf97f8117fe6ea918d86e8ee1bfcb09a9889231
Filipe Marques Hottis <lipehottis@gmail.com>
2023-10-03 19:50:31 -0300

    feat: Novo Json

--- src/routes/activities.router.js
+++ src/routes/activities.router.js
@@ -1,7 +1,15 @@
 const { Router } = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const router = Router();
+const pathActivities = '../json/activities.json';
 
+router.get('/activities', async (req, res) => {
+    const data = await fs.readFile(path.resolve(__dirname, pathActivities));
+    const dataJson = JSON.parse(data);
+    res.status(200).json(dataJson);
+});
 router.post('/activities', (req, res) => {
     res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
 });
